4819f15b58294f76aeb18bbb2cdb6214dff58be9
Sukh Singh <sukh@orbs.com>
2023-08-30 18:03:23 +0100

    initial setup of react-static website

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# website
+# Excited DAO Website
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>
+      Welcome to Excited DAO
+    </h1>
+
+  </div>
+)
