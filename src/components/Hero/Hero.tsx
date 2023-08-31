d273ca44e5b1e179450887c270662500d0ec78d8
Sukh Singh <sukh@orbs.com>
2023-08-31 20:51:50 +0100

    added fonts + some styling

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,8 +2,13 @@ import './Hero.css';
 
 export function Hero() {
   return (
-    <div className="p-5 hero-container">
+    <div className="hero-container">
       <img className="logo" src="/images/xctd-logo.svg" alt="Excited" />
+      <h1 className="text-2xl md:text-4xl lg:text-5xl title">
+        Building the biggest Investment
+        <br />
+        Platform in the market
+      </h1>
     </div>
   );
 }
