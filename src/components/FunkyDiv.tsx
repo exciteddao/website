040095eded19cd9aa6e0100e912e75a3670659f7
Sukh Singh <sukh@orbs.com>
2023-08-31 20:11:56 +0100

    added liniting, hero cmp, logo

--- src/components/FunkyDiv.tsx
+++
@@ -1,3 +0,0 @@
-export function FunkyDiv({ children }: { children: React.ReactNode }) {
-  return <div className="text-5xl">{children}</div>;
-}
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+import './Hero.css';
+
+export function Hero() {
+  return (
+    <div className="p-5 hero-container">
+      <img className="logo" src="/images/xctd-logo.svg" alt="Excited" />
+    </div>
+  );
+}
