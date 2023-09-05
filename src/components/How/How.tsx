4e8d6b9264af29a0d830588f148f320106ddd36a
Sukh Singh <sukh@orbs.com>
2023-09-05 16:04:13 +0100

    added how section

---
+++ src/components/How/How.tsx
@@ -0,0 +1,10 @@
+import './How.css';
+import { t } from 'i18next';
+
+export function How() {
+  return (
+    <section className="how">
+      <h2>{t('how.title')}</h2>
+    </section>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './Hero';
 export * from './Features';
 export * from './Header';
 export * from './What';
+export * from './How';
