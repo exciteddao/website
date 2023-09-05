27b97226bd3726b2cc1717e04a5ed73e5028b459
Sukh Singh <sukh@orbs.com>
2023-09-05 13:55:37 +0100

    added images to features

--- src/components/Features/Feature.tsx
+++ src/components/Features/Feature.tsx
@@ -3,18 +3,28 @@ import './feature.css';
 type FeatureProps = {
   title: string;
   description: string;
-  link: string;
+  linkUrl: string;
   linkText: string;
+  imageUrl: string;
 };
 
-export function Feature({ title, description, link, linkText }: FeatureProps): React.ReactElement {
+export function Feature({
+  title,
+  description,
+  linkUrl,
+  linkText,
+  imageUrl,
+}: FeatureProps): React.ReactElement {
   return (
     <div className="flex flex-col justify-between items-center gap-8 feature">
+      <div className="max-w-[100px] md:max-w-[30%] image">
+        <img src={imageUrl} alt={title} />
+      </div>
       <div>
         <h3 className="title">{title}</h3>
         <p className="description">{description}</p>
       </div>
-      <a className="shrink-1 cta" href={link}>
+      <a className="shrink-1 cta" href={linkUrl}>
         {linkText}
       </a>
     </div>
--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -3,28 +3,31 @@ import { Feature } from './Feature';
 
 export function Features(): React.ReactElement {
   return (
-    <div className="flex flex-row items-stretch gap-8 mt-32">
-      <div className="basis-1/3">
+    <div className="flex flex-row flex-wrap md:flex-nowrap items-stretch gap-8 mt-32">
+      <div className="basis-full md:basis-1/3 mb-16">
         <Feature
+          imageUrl={t('features.feature1.imageUrl')}
           title={t('features.feature1.title')}
           description={t('features.feature1.description')}
-          link={t('features.feature1.linkUrl')}
+          linkUrl={t('features.feature1.linkUrl')}
           linkText={t('features.feature1.linkText')}
         />
       </div>
-      <div className="basis-1/3">
+      <div className="basis-full md:basis-1/3 mb-16">
         <Feature
+          imageUrl={t('features.feature2.imageUrl')}
           title={t('features.feature2.title')}
           description={t('features.feature2.description')}
-          link={t('features.feature2.linkUrl')}
+          linkUrl={t('features.feature2.linkUrl')}
           linkText={t('features.feature2.linkText')}
         />
       </div>
-      <div className="basis-1/3">
+      <div className="basis-full md:basis-1/3 mb-16">
         <Feature
+          imageUrl={t('features.feature3.imageUrl')}
           title={t('features.feature3.title')}
           description={t('features.feature3.description')}
-          link={t('features.feature3.linkUrl')}
+          linkUrl={t('features.feature3.linkUrl')}
           linkText={t('features.feature3.linkText')}
         />
       </div>
