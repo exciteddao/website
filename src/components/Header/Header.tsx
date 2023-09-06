9de6ef2b845668968cef2a86fa3635a168ec00ee
Sukh Singh <sukh@orbs.com>
2023-09-06 13:46:32 +0100

    removed react code and moved to vanilla js

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,10 @@ import { Logo } from '../Logo';
 import './Header.css';
 import { DesktopNavMenu } from './DesktopNavMenu';
 import { MobileNavMenu } from './MobileNavMenu';
-import { useState } from 'react';
 
 const menuItems: Array<{ text: string; url: string }> = t('header.menu', { returnObjects: true });
 
 export function Header(): React.ReactElement {
-  const [open, setOpen] = useState(false);
-
-  const hamburgerStyles = `block sm:hidden self-end hamburger ${open ? 'open' : ''}`;
 
   return (
     <header className="flex justify-between items-center md:items-end header">
@@ -18,8 +14,8 @@ export function Header(): React.ReactElement {
         <Logo className="text-white w-full" />
       </div>
       <DesktopNavMenu menuItems={menuItems} />
-      <MobileNavMenu menuItems={menuItems} open={open} onClose={() => setOpen(false)} />
-      <button onClick={() => setOpen(!open)} className={hamburgerStyles}>
+      <MobileNavMenu menuItems={menuItems} />
+      <button id="xctd-menu" className="block sm:hidden self-end hamburger">
         <span></span>
         <span></span>
         <span></span>
--- src/components/Header/MobileNavMenu.tsx
+++ src/components/Header/MobileNavMenu.tsx
@@ -1,40 +1,23 @@
 import { t } from 'i18next';
 import { TelegramButton } from '../TelegramButton';
-import { useEffect } from 'react';
 import { Logo } from '../Logo';
 import type { TMenuItem } from './types';
 
 type MobileNavMenuProps = {
   menuItems: TMenuItem[];
-  open: boolean;
-  onClose: () => void;
 };
 
-export function MobileNavMenu({ menuItems, open, onClose }: MobileNavMenuProps) {
+export function MobileNavMenu({ menuItems }: MobileNavMenuProps) {
   const Menu = menuItems.map(({ text, url }) => (
     <li key={url}>
-      <a href={url} onClick={onClose} className="link">
+      <a href={url} className="link mobile-nav-link">
         {text}
       </a>
     </li>
   ));
 
-  const navStyles = `block sm:hidden mobile-nav ${open ? 'mobile-nav-in' : ''}`;
-
-  useEffect(() => {
-    if (open) {
-      document.body.style.position = 'fixed';
-      document.body.style.top = `-${window.scrollY}px`;
-    } else {
-      const scrollY = document.body.style.top;
-      document.body.style.position = '';
-      document.body.style.top = '';
-      window.scrollTo(0, parseInt(scrollY || '0') * -1);
-    }
-  }, [open]);
-
   return (
-    <nav className={navStyles}>
+    <nav className="block sm:hidden mobile-nav">
       <ul className="flex flex-col items-center gap-16">
         <Logo className="text-[#4a4a4a]" />
         {Menu}
