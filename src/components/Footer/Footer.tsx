160cc6ff1b3944ef2bd6fad1e0e3887513def5a1
Sukh Singh <sukh@orbs.com>
2023-09-08 14:38:02 +0100

    updated footer design

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,27 +3,43 @@ import { Trans } from 'react-i18next';
 
 import './Footer.css';
 import { TelegramButton } from '../TelegramButton';
+import { t } from 'i18next';
+
+const menuItems: Array<{ text: string; url: string }> = t('header.menu', { returnObjects: true });
+const Menu = menuItems.map(({ text, url }) => (
+  <li key={url}>
+    <a href={url} className="link">
+      {text}
+    </a>
+  </li>
+));
 
 export function Footer() {
   return (
-    <section className="footer">
-      <div className="flex flex-wrap justify-center items-center">
-        <div className="md:basis-1/4 hidden md:block">
-          <img src="/website/images/plant-bot.png" alt="plant bot" />
-        </div>
-        <footer className="md:basis-1/2 flex flex-col items-center gap-8">
-          <Logo className="text-[#AD73FC]" />
-          <h3 className="text-center">
+    <section className="xl:px-[120px] footer">
+      <footer className="flex flex-wrap justify-between ">
+        <div className='basis-1/2 flex flex-col gap-6'>
+          <Logo className="text-[#fff]" />
+          <h3>
             <Trans i18nKey="footerText">
-              Building the biggest <span>Investment Platform</span> in the market
+              Building the <span>biggest Investment Platform</span> in the market
             </Trans>
           </h3>
-          <TelegramButton className="text-[#AD73FC]" width={44} height={44} />
-        </footer>
-        <div className="md:basis-1/4 hidden md:block">
-          <img src="/website/images/plant-bot2.png" alt="plant bot 2" />
-        </div>
-      </div>
+          </div>
+        <nav className="hidden sm:block">
+          <ul className="flex flex-col items-end gap-6">
+            {Menu}
+            <li>
+              <TelegramButton className="text-white" />
+            </li>
+            <li>
+              <a href={t('header.registerButton.url')} className="button">
+                {t('header.registerButton.text')}
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
     </section>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,6 +8,8 @@ const menuItems: Array<{ text: string; url: string }> = t('header.menu', { retur
 
 export function Header(): React.ReactElement {
 
+
+
   return (
     <header className="flex justify-between items-center md:items-end p-4 sm:px-12 header">
       <div className="max-w-[30%] md:max-w-full mr-8">
